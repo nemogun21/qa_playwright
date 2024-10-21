f73dbd1c53b0cc6cef57cb3f8161122845db2eee
nemogun21 <41777904+nemogun21@users.noreply.github.com>
2024-10-21 18:26:40 +0900

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -19,7 +19,7 @@ const { expect, test, devices } = require('@playwright/test');
 // });
 
 test('SonarQube > 로그인 페이지 진입 > 로그인 수행 > Progect 화면 노출 > 페이지 닫기', async ({ page }) => {
-  await page.goto('http://127.0.0.1:9000/');
+  await page.goto('http://223.131.50.184:9000/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("SonarQube");
